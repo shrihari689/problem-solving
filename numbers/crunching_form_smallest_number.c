2a18b7b4ad21148073ba078447e85059dde20e49
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2022-03-15 23:52:51 +0530

    🌞 Crunching Form Smallest Number

---
+++ numbers/crunching_form_smallest_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int check_digit_inside(long long int n, int digit)
+{
+    while (n > 0)
+    {
+        int rem = n % 10;
+        if (rem == digit)
+            return 1;
+        n /= 10;
+    }
+    return 0;
+}
+
+int main()
+{
+    long int result = 0;
+    long long int n;
+    scanf("%ld", &n);
+
+    for (int i = 1; i <= 9; ++i)
+    {
+        if (check_digit_inside(n, i))
+        {
+            result = result * 10 + i;
+        }
+    }
+
+    printf("%ld", result);
+    return 0;
+}
