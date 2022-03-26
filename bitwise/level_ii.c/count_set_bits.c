de938e70682d167368d89f905236e82a24fc1f0a
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2022-03-26 21:21:00 +0530

    📦 Started Bitwise

---
+++ bitwise/level_ii.c/count_set_bits.c
@@ -0,0 +1,22 @@
+// Write a program to count number of bits set to 1 in an Integer.
+
+#include <stdio.h>
+
+int main()
+{
+
+    long int n;
+    scanf("%ld", &n);
+
+    int count = 0;
+
+    while (n > 0)
+    {
+        count += n & 1;
+        n = n >> 1;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
